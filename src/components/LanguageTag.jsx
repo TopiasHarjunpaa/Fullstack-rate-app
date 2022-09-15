6e41dccbad9e4e2f63543eb8ed7c2406da3ccf37
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-09-15 21:24:37 +0300

    exercise 10.5

--- src/components/LanguageTag.jsx
+++ src/components/LanguageTag.jsx
@@ -1,25 +1,24 @@
-import { View, StyleSheet } from 'react-native';
-import Text from './Text';
-import theme from '../theme';
-
+import { View, StyleSheet } from "react-native";
+import Text from "./Text";
+import theme from "../theme";
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: theme.colors.languageTagBackground,
+  container: {
+    backgroundColor: theme.colors.languageTagBackground,
     borderRadius: 5,
-		padding: 5,
-		alignSelf: 'flex-start'
-	},
+    padding: 6,
+    alignSelf: "flex-start",
+  },
 });
 
 const languageTag = ({ language }) => {
-	return (
-		<View style={styles.container}>
-			<Text color='textSecondary' fontSize='subheading'>
-				{language}
-			</Text>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text color="textSecondary" fontSize="subheading">
+        {language}
+      </Text>
+    </View>
+  );
 };
 
-export default languageTag
+export default languageTag;
