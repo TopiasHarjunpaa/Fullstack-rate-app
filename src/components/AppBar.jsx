2e19c21a5b630b81a3216a327a1903c2a5ec6f05
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-09-15 21:21:09 +0300

    exercise 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,11 +1,11 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 
 import theme from "../theme";
 
 const styles = StyleSheet.create({
-  flexContainer: {
+  appBar: {
     display: "flex",
     paddingTop: Constants.statusBarHeight,
     flexDirection: "row",
@@ -17,9 +17,11 @@ const styles = StyleSheet.create({
 
 const AppBar = () => {
   return (
-    <View style={styles.flexContainer}>
-      <AppBarTab name={"Repositories"} route={"/"} />
-      <AppBarTab name={"Sign in"} route={"/signin"} />
+    <View style={styles.appBar}>
+      <ScrollView horizontal>
+        <AppBarTab name={"Repositories"} route={"/"} />
+        <AppBarTab name={"Sign in"} route={"/signin"} />
+      </ScrollView>
     </View>
   );
 };
