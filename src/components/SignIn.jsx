182bfa6041cd2a4036cbb77590ad92c3257b1d32
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-09-16 18:55:36 +0300

    exercise 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,19 @@
-import Text from "./Text";
+import { Formik } from "formik";
+import SignInForm from "./SignInForm";
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => onSubmit(values)}
+    >
+      {({ handleSubmit }) => <SignInForm handleSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,25 @@
 const theme = {
-    colors: {
-      textPrimary: '#24292e',
-      textSecondary: 'white',
-      primary: '#0366d6',
-      appBarBackground: '#24292e',
-      mainBackground: '#e1e4e8',
-      repositoryItemBackground: 'white',
-      languageTagBackground: '#0366d6'
-    },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-    },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400',
-      bold: '700',
-    },
-  };
-  
-  export default theme;
+  colors: {
+    textPrimary: "#24292e",
+    textSecondary: "white",
+    primary: "#0366d6",
+    secondary: "grey",
+    appBarBackground: "#24292e",
+    mainBackground: "#e1e4e8",
+    repositoryItemBackground: "white",
+    languageTagBackground: "#0366d6",
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: "System",
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "700",
+  },
+};
+
+export default theme;
